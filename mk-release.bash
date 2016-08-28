77b3b705d5fd3e74434e5c335ec95c6d88a8b325
R. S. Doiel <rsdoiel@gmail.com>
2016-08-28 10:13:21 -0700

    quick save

--- mk-release.bash
+++ mk-release.bash
@@ -3,12 +3,18 @@
 # Make releases for Linux/amd64, Linux/ARM6 and Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
 #
 RELEASE_NAME=mkpage
+echo "NOTE: this can take a while..."
 for PROGNAME in mkpage reldocpath; do
-  env GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-  env GOOS=darwin GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-  env GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-  env GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-  env GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
+    echo "Building $PROGNAME dist/linix-amd64"
+  env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    echo "Building $PROGNAME dist/maxosx-amd64"
+  env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    echo "Building $PROGNAME dist/raspberrypi-arm6"
+  env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    echo "Building $PROGNAME dist/raspberrypi-arm7"
+  env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
+    echo "Building $PROGNAME dist/windows-amd64"
+  env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
 done
 
 zip -r "$RELEASE_NAME-binary-release.zip" README.md INSTALL.md LICENSE dist/*
