3bab2088049d22383af0ede80a249f78eaa3228e
R. S. Doiel <rsdoiel@gmail.com>
2016-08-28 10:33:18 -0700

    quick save

--- mk-release.bash
+++ mk-release.bash
@@ -5,15 +5,15 @@
 RELEASE_NAME=mkpage
 echo "NOTE: this can take a while..."
 for PROGNAME in mkpage reldocpath; do
-    echo "Building $PROGNAME dist/linix-amd64"
+    echo "Building dist/linix-amd64/$PROGNAME"
   env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-    echo "Building $PROGNAME dist/maxosx-amd64"
+    echo "Building dist/maxosx-amd64/$PROGNAME"
   env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-    echo "Building $PROGNAME dist/raspberrypi-arm6"
+    echo "Building dist/raspberrypi-arm6/$PROGNAME"
   env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -o dist/raspberrypi-arm6/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-    echo "Building $PROGNAME dist/raspberrypi-arm7"
+    echo "Building dist/raspberrypi-arm7/$PROGNAME"
   env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
-    echo "Building $PROGNAME dist/windows-amd64"
+    echo "Building dist/windows-amd64/$PROGNAME"
   env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
 done
 
