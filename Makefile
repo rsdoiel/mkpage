f603268630c1a3386c920814f943d8bc553b405f
R. S. Doiel <rsdoiel@gmail.com>
2016-12-21 11:26:17 -0800

    quick save

--- Makefile
+++ Makefile
@@ -1,12 +1,14 @@
 #
 # Simple Makefile
 #
+PROJECT = mkpage
+
+VERSION = $(shell grep -m 1 'Version =' $(PROJECT).go | cut -d\"  -f 2)
+
+BRANCH = $(shell git branch | grep '* ' | cut -d\  -f 2)
+
 build:
-	gofmt -w mkpage.go
-	gofmt -w mkpage_test.go
-	gofmt -w cmds/mkpage/mkpage.go
-	go build
-	go build -o bin/mkpage cmds/mkpage/mkpage.go
+	env CGO_ENABLED=0 go build -o bin/mkpage cmds/mkpage/mkpage.go
 
 lint:
 	golint mkpage.go
@@ -16,17 +18,19 @@ test:
 	go test
 
 save:
-	./mk-website.bash
+	gofmt -w mkpage.go
+	gofmt -w mkpage_test.go
+	gofmt -w cmds/mkpage/mkpage.go
 	git commit -am "quick save"
-	git push origin master
+	git push origin $(VERSION)
 
 clean:
 	if [ -d bin ]; then rm -fR bin; fi
 	if [ -d dist ]; then rm -fR dist; fi
-	if [ -f mkpage-release.zip ]; then rm -f mkpage-release.zip; fi
+	if [ -f $(PROJECT)-$(VERSION)-release.zip ]; then rm -f $(PROJECT)-$(VERSION)-release.zip; fi
 
 install:
-	env GOBIN=$(HOME)/bin go install cmds/mkpage/mkpage.go
+	env CGO_ENABLED=0 GOBIN=$(HOME)/bin go install cmds/mkpage/mkpage.go
 
 release:
 	./mk-release.bash
--- mk-release.bash
+++ mk-release.bash
@@ -2,7 +2,10 @@
 #
 # Make releases for Linux/amd64, Linux/ARM6 and Linux/ARM7 (Raspberry Pi), Windows, and Mac OX X (darwin)
 #
-RELEASE_NAME=mkpage
+PROJECT=mkpage
+VERSION=$(grep -m 1 'Version =' $PROJECT.go | cut -d\" -f 2)
+RELEASE_NAME=$PROJECT-$VERSION
+echo "Preparing release $RELEASE_NAME"
 for PROGNAME in mkpage; do
   env CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
   env CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/macosx-amd64/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
@@ -10,5 +13,9 @@ for PROGNAME in mkpage; do
   env CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -o dist/raspberrypi-arm7/$PROGNAME cmds/$PROGNAME/$PROGNAME.go
   env CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/windows-amd64/$PROGNAME.exe cmds/$PROGNAME/$PROGNAME.go
 done
-
-zip -r "$RELEASE_NAME-release.zip" README.md INSTALL.md LICENSE dist/*
+mkdir -p dist
+for FNAME in README.md LICENSE INSTALL.md; do
+  cp -v $FNAME dist/
+done
+echo "Zipping $RELEASE_NAME"
+zip -r "$RELEASE_NAME-release.zip" dist/*
