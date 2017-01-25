c57f8d265ff99d5362f719b14c2e238c3b4b8f7c
R. S. Doiel <rsdoiel@gmail.com>
2017-01-25 11:36:43 -0800

    Added sitemapper command

--- mkpage.go
+++ mkpage.go
@@ -36,7 +36,7 @@ import (
 
 const (
 	// Version of the mkpage package.
-	Version = "v0.0.12-alpha1"
+	Version = "v0.0.12"
 
 	// LicenseText provides a string template for rendering cli license info
 	LicenseText = `
