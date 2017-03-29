deaba69b8fc73314da6d7acaa7d4d07fc79419c1
R. S. Doiel <rsdoiel@gmail.com>
2017-03-29 13:35:05 -0700

    Quick Save

--- mkpage.go
+++ mkpage.go
@@ -3,7 +3,7 @@
 //
 // @author R. S. Doiel, <rsdoiel@caltech.edu>
 //
-// Copyright (c) 2016, Caltech
+// Copyright (c) 2017, Caltech
 // All rights not granted herein are expressly reserved by Caltech.
 //
 // Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -39,7 +39,7 @@ import (
 
 const (
 	// Version of the mkpage package.
-	Version = "v0.0.16"
+	Version = "v0.0.17"
 
 	// LicenseText provides a string template for rendering cli license info
 	LicenseText = `
