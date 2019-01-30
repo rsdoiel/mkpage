c5f4c153eeaa8c41ef4a7e9d03010df190207690
R. S. Doiel <rsdoiel@gmail.com>
2019-01-30 08:55:39 -0800

    Version bump for release of recompiled binaries using go1.11.5

--- mkpage.go
+++ mkpage.go
@@ -39,7 +39,7 @@ import (
 
 const (
 	// Version of the mkpage package.
-	Version = `v0.0.22`
+	Version = `v0.0.23`
 
 	// LicenseText provides a string template for rendering cli license info
 	LicenseText = `
