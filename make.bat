42d2b8f1f2a170ee4bd6933bc745d472530be53e
R. S. Doiel <rsdoiel@gmail.com>
2019-04-19 15:55:12 -0700

    snapshot

--- make.bat
+++ make.bat
@@ -6,6 +6,7 @@ REM It requires: go version 1.12.4 or better and the cli for git installed
 REM
 go version
 echo "Getting ready to build the commands and write them to .\bin"
+@echo on
 
 go build -o bin\byline.exe cmd\byline\byline.go
 go build -o bin\mkpage.exe cmd\mkpage\mkpage.go
@@ -18,4 +19,5 @@ go build -o bin\urldecode.exe cmd\urldecode\urldecode.go
 go build -o bin\urlencode.exe cmd\urlencode\urlencode.go
 go build -o bin\ws.exe cmd\ws\ws.go
 
+@echo off
 echo "You can now copy the contents of .\bin to your program directory"
