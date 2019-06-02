04a069328d9c665b83bb7a6e8a294b85d06c990e
R. S. Doiel <rsdoiel@gmail.com>
2019-06-02 10:30:28 -0700

    bump version number in prep to v0.0.27 release

--- mkpage.go
+++ mkpage.go
@@ -40,7 +40,7 @@ import (
 
 const (
 	// Version of the mkpage package.
-	Version = `v0.0.26`
+	Version = `v0.0.27`
 
 	// LicenseText provides a string template for rendering cli license info
 	LicenseText = `
