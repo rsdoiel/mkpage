fb8783c4c0e0be4cf3a75d2746050849ccb08203
R. S. Doiel <rsdoiel@gmail.com>
2019-07-27 10:05:09 -0700

    removed Let's Encyrpt support, certbot works better, update wsfn support in ws cli

--- mkpage.go
+++ mkpage.go
@@ -55,7 +55,7 @@ import (
 
 const (
 	// Version holds the semver assocaited with this version of mkpage.
-	Version = `v0.0.29`
+	Version = `v0.0.30`
 
 	// LicenseText provides a string template for rendering cli license info
 	LicenseText = `
