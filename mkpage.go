bb6221e9d69569f2a8fc0f2be4fc2db1f8ba35db
R. S. Doiel <rsdoiel@gmail.com>
2019-09-10 11:21:27 -0700

    bumped version number to v0.0.31, dropped blackfriday support

--- mkpage.go
+++ mkpage.go
@@ -56,7 +56,7 @@ import (
 
 const (
 	// Version holds the semver assocaited with this version of mkpage.
-	Version = `v0.0.30`
+	Version = `v0.0.31`
 
 	// LicenseText provides a string template for rendering cli license info
 	LicenseText = `
